4681ac43b5c39afbfa559462b92012dee8ebc8ee
Oisín Smyth <oisin@shanik.net>
2025-12-22 21:41:38 +0000

    Basic booking system of hardcoded time slots + README

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# tefl-booking-system
+# TEFL Booking System
+
+This project is a personal learning portfolio project exploring backend dev with Flask through designing a lesson booking system for TEFL (Teaching English as a Foreign Language) lessons. The aim of this system is to manage availability and lesson bookings so that it is robust regardless of time zones and scheduling conflicts, reflecting the real world constraints when teaching students across regions.
+
+## Methods
+
+- Language: Python
+- Framework: Flask
+- Data Storage (Future): SQLite
+- Environment: Conda, VSCode
+
+## Current Functionality
+
+- Define available time slots manually
+- View available slots through a REST API
+- Book lessons through a POST request
+- Prevent double booking and overlapping time slots
+
+## Planned extensions
+
+- Persistent storage with SQLite
+- Time zone handling 
+- Cancellation of bookings
+- Comparative scheduling views
+- Payment system
--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, send_file
 from datetime import datetime, timedelta, timezone
+import os
 
 app = Flask(__name__)
 
+#Hardcoded availability calendar for testing
 available_slots = [
     {
         "start": "2025-12-23T14:00:00Z",
@@ -14,13 +16,49 @@ available_slots = [
     },
 ]
 
+# Creating the home page of website
 @app.route('/')
 def home():
-    return "Booking system initalised."
+    return "Booking System Initialised."
+    #"""return
+    #<h1>Mi novia thalia es muy hermosa <3</h1>
+    #<img src="/image" alt="mi corazón" style="max-width: 500px;">
+    #"""
 
-@app.route('/availability')
+#@app.route('/image')
+#def get_image():
+#    image_path = os.path.join(os.path.dirname(__file__), 'images/micorazón.jpg')
+#    return send_file(image_path, mimetype='image/jpeg')
+
+
+# Creates the availability page to show available timeslots
+@app.route('/availability', methods=['GET'])
 def availability():
     return jsonify(available_slots)
+
+# Booking page to select 
+@app.route('/book', methods=['POST'])
+def book():
+    data = request.json
+
+    if not data or "start" not in data or "end" not in data:
+        return jsonify({"error": "Missing start or end time"}), 400
+    
+    requested_slot = {
+        "start": data["start"],
+        "end": data["end"]
+    }
+
+    if requested_slot not in available_slots:
+        return jsonify({"error": "requested time slot is not available"}), 409
+    
+    available_slots.remove(requested_slot)
+
+    return jsonify({
+        "message": "Lesson successfully booked",
+        "slot": requested_slot
+    }), 200
+
 
 if __name__ == "__main__":
     app.run(debug="True")
