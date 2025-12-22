343893bde76574a5c4cd16366941f06755904635
Oisín Smyth <oisin@shanik.net>
2025-12-22 18:52:14 +0000

    Add availability endpoint returning time slots

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
+from datetime import datetime, timedelta, timezone
 
 app = Flask(__name__)
 
-@app.route('/')
+available_slots = [
+    {
+        "start": "2025-12-23T14:00:00Z",
+        "end": "2025-12-23T15:00:00Z"
+    },
+    {
+        "start": "2025-12-23T15:30:00Z",
+        "end": "2025-12-23T16:30:00Z"
+    },
+]
 
+@app.route('/')
 def home():
     return "Booking system initalised."
+
+@app.route('/availability')
+def availability():
+    return jsonify(available_slots)
 
 if __name__ == "__main__":
     app.run(debug="True")
